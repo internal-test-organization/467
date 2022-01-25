8c0eab1a0656800c539eb561ae5cb9c0fc44b478
mosesbalaji <94154646+mosesbalaji@users.noreply.github.com>
2022-01-25 14:15:27 +0530

    Update Organization.js

--- src/githublib/Organization.js
+++ src/githublib/Organization.js
@@ -90,8 +90,11 @@ module.exports = class Organization {
       return this.octokit.paginate('GET /repos/{owner}/{repo}/actions/runs', {owner: org,repo: reponame,per_page: 100})
       .then(workflowruns => {
         console.log(`Processing ${workflowruns.length} workflow runs`);
-        console.log(workflowruns)
-        return workflowruns.total_count
+        return workflowruns.map(workflowrun => {
+          return {
+            name: workflowrun.node_id,
+          };
+        });
       });
     }
     getOrgs(org) {
