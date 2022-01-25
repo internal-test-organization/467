87f856e94cacd29eb434c6a72ee61aebdd9d5b59
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-25 16:56:09 +0800

    467

--- dist/index.js
+++ dist/index.js
@@ -13457,7 +13457,7 @@ for(org of orgs){
         workflowruns = await orgActivity1.getWorkFlowRuns(org,repos);
         console.log(workflowruns,"workflow runs total count")
         workflowruns.map((item) => {
-             workflowrun.map(item)
+             workflowrun.push(item)
          })
     }
 }
