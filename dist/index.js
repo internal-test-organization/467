1dc78400cae1a80053517fb091caa75d7d08c8c3
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-27 23:21:20 +0800

    467

--- dist/index.js
+++ dist/index.js
@@ -14112,75 +14112,75 @@ async function run() {
     console.log(acworkflows,"adhoc workflows")
     console.log(acrepolist,"adhoc repolist")
     ///*******filter using dates */
-    const filteredrepos = acrepolist?.filter(function(data){
-            const releaseYear = new Date(data.releaseDate).getFullYear();
-            return (
-                releaseYear >= new Date(FromDate).getFullYear() &&
-                releaseYear <= new Date(toDate).getFullYear()
-            );
-    })  
-        filteredrepos = acrepolist?.filter(function(data){
-            const releaseMonth = new Date(data.releaseMonth).getMonth();
-            return (
-                releaseMonth >= new Date(FromDate).getMonth() &&
-                releaseMonth <= new Date(toDate).getMonth()
-            );
-    })
-        filteredrepos = acrepolist?.filter(function(data){
-            const releaseDate = new Date(data.releaseDate).getDate();
-            return (
-                releaseDate >= new Date(FromDate).getDate() &&
-                releaseDate <= new Date(toDate).getDate()
-            );
-    })
-    //*****workflowrun */
-    const filteredwfruns = acworkflowruns?.filter(function(data){
-        const releaseYear = new Date(data.releaseDate).getFullYear();
-        return (
-            releaseYear >= new Date(FromDate).getFullYear() &&
-            releaseYear <= new Date(toDate).getFullYear()
-        );
-    })
-    filteredwfruns = acworkflowruns?.filter(function(data){
-        const releaseMonth = new Date(data.releaseMonth).getMonth();
-        return (
-            releaseMonth >= new Date(FromDate).getMonth() &&
-            releaseMonth <= new Date(toDate).getMonth()
-        );
-    })
-    filteredwfruns = acworkflowruns?.filter(function(data){
-        const releaseDate = new Date(data.releaseDate).getDate();
-        return (
-            releaseDate >= new Date(FromDate).getDate() &&
-            releaseDate <= new Date(toDate).getDate()
-        );
-    })
-    //***********workflows */
-    const filteredworkflows = acworkflows?.filter(function(data){
-        const releaseYear = new Date(data.releaseDate).getFullYear();
-        return (
-            releaseYear >= new Date(FromDate).getFullYear() &&
-            releaseYear <= new Date(toDate).getFullYear()
-        );
-    })
-    filteredworkflows = acworkflows?.filter(function(data){
-        const releaseMonth = new Date(data.releaseMonth).getMonth();
-        return (
-            releaseMonth >= new Date(FromDate).getMonth() &&
-            releaseMonth <= new Date(toDate).getMonth()
-        );
-    })
-    filteredworkflows = acworkflows?.filter(function(data){
-        const releaseDate = new Date(data.releaseDate).getDate();
-        return (
-            releaseDate >= new Date(FromDate).getDate() &&
-            releaseDate <= new Date(toDate).getDate()
-        );
-    })
+    // const filteredrepos = acrepolist?.filter(function(data){
+    //         const releaseYear = new Date(data.releaseDate).getFullYear();
+    //         return (
+    //             releaseYear >= new Date(FromDate).getFullYear() &&
+    //             releaseYear <= new Date(toDate).getFullYear()
+    //         );
+    // })  
+    //     filteredrepos = acrepolist?.filter(function(data){
+    //         const releaseMonth = new Date(data.releaseMonth).getMonth();
+    //         return (
+    //             releaseMonth >= new Date(FromDate).getMonth() &&
+    //             releaseMonth <= new Date(toDate).getMonth()
+    //         );
+    // })
+    //     filteredrepos = acrepolist?.filter(function(data){
+    //         const releaseDate = new Date(data.releaseDate).getDate();
+    //         return (
+    //             releaseDate >= new Date(FromDate).getDate() &&
+    //             releaseDate <= new Date(toDate).getDate()
+    //         );
+    // })
+    // //*****workflowrun */
+    // const filteredwfruns = acworkflowruns?.filter(function(data){
+    //     const releaseYear = new Date(data.releaseDate).getFullYear();
+    //     return (
+    //         releaseYear >= new Date(FromDate).getFullYear() &&
+    //         releaseYear <= new Date(toDate).getFullYear()
+    //     );
+    // })
+    // filteredwfruns = acworkflowruns?.filter(function(data){
+    //     const releaseMonth = new Date(data.releaseMonth).getMonth();
+    //     return (
+    //         releaseMonth >= new Date(FromDate).getMonth() &&
+    //         releaseMonth <= new Date(toDate).getMonth()
+    //     );
+    // })
+    // filteredwfruns = acworkflowruns?.filter(function(data){
+    //     const releaseDate = new Date(data.releaseDate).getDate();
+    //     return (
+    //         releaseDate >= new Date(FromDate).getDate() &&
+    //         releaseDate <= new Date(toDate).getDate()
+    //     );
+    // })
+    // //***********workflows */
+    // const filteredworkflows = acworkflows?.filter(function(data){
+    //     const releaseYear = new Date(data.releaseDate).getFullYear();
+    //     return (
+    //         releaseYear >= new Date(FromDate).getFullYear() &&
+    //         releaseYear <= new Date(toDate).getFullYear()
+    //     );
+    // })
+    // filteredworkflows = acworkflows?.filter(function(data){
+    //     const releaseMonth = new Date(data.releaseMonth).getMonth();
+    //     return (
+    //         releaseMonth >= new Date(FromDate).getMonth() &&
+    //         releaseMonth <= new Date(toDate).getMonth()
+    //     );
+    // })
+    // filteredworkflows = acworkflows?.filter(function(data){
+    //     const releaseDate = new Date(data.releaseDate).getDate();
+    //     return (
+    //         releaseDate >= new Date(FromDate).getDate() &&
+    //         releaseDate <= new Date(toDate).getDate()
+    //     );
+    // })
 
-    console.log(filteredrepos,"filtered repos with date month and year")
-    console.log(filteredwfruns,"filtered workflowruns with date month and year")
-    console.log(filteredworkflows,"filtered workflows with date month and year")
+    // console.log(filteredrepos,"filtered repos with date month and year")
+    // console.log(filteredwfruns,"filtered workflowruns with date month and year")
+    // console.log(filteredworkflows,"filtered workflows with date month and year")
 
     ///*******filter using dates */
 
@@ -14194,9 +14194,9 @@ async function run() {
         }
     }
     let finaloutput = [];
-    finaloutput.push({"total_orgs": orgs.length,"total_users":uniqueUsers.length,"active_users": activeuser.length,"total_repos":filteredrepos.length,"total_workflow_runs":filteredwfruns.length ,"total_workflows":filteredworkflows.length})
-    finaloutputresult = JSON.stringify(finaloutput)
-    console.log(finaloutput)
+    // finaloutput.push({"total_orgs": orgs.length,"total_users":uniqueUsers.length,"active_users": activeuser.length,"total_repos":filteredrepos.length,"total_workflow_runs":filteredwfruns.length ,"total_workflows":filteredworkflows.length})
+    // finaloutputresult = JSON.stringify(finaloutput)
+    // console.log(finaloutput)
     
     saveIntermediateData(outputDir, finaloutput);
     
