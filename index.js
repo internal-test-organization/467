ae1787dbb6fa601735ff6a274e2fd72a75de22b9
mosesbalaji <moses.balaji-external@tcs.com>
2022-02-01 15:59:41 +0800

    467

--- index.js
+++ index.js
@@ -32,11 +32,8 @@ async function run() {
         throw new Error('Provide a valid activity_days - It accept only Positive Number');
       }
     
-      let regex = /^[\w\.\_\-]+((,|-)[\w\.\_\-]+)*[\w\.\_\-]+$/g;
-      let validate_org = regex.test(organizationinp);
-      if((!validate_org)) {
-        throw new Error('Provide a valid organization - It accept only comma separated value');
-      }
+      
+      
     
       let sinceregex = /^(20)\d\d-(0[1-9]|1[012])-([012]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/ 
     ;
@@ -178,11 +175,7 @@ async function run() {
         throw new Error('Provide a valid activity_days - It accept only Positive Number');
       }
     
-      let regex = /^[\w\.\_\-]+((,|-)[\w\.\_\-]+)*[\w\.\_\-]+$/g;
-      let validate_org = regex.test(organizationinp);
-      if((!validate_org)) {
-        throw new Error('Provide a valid organization - It accept only comma separated value');
-      }
+      
     
       let sinceregex = /^(20)\d\d-(0[1-9]|1[012])-([012]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/ 
     ;
